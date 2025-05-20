f97c247b9563ed5bd572903071b425cfb9e593e8
alocshrestha <aloc.shrestha@gmail.com>
2025-05-20 14:44:01 -0700

    JdbcTemplate vs SimpleJdbcInsert

--- src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
+++ src/main/java/com/pluralsight/conference/repository/SpeakerRepositoryImpl.java
@@ -2,21 +2,24 @@ package com.pluralsight.conference.repository;
 
 import com.pluralsight.conference.model.Speaker;
 import org.springframework.stereotype.Repository;
-//import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Repository("speakerRepository")
 public class SpeakerRepositoryImpl implements SpeakerRepository {
 
-    /*
+
     //to inject or autowire a jdbc template
     private JdbcTemplate jdbcTemplate;
     //to autowire - setup param constructor
     public SpeakerRepositoryImpl(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
-     */
+
 
 
     public List<Speaker> findAll() {
@@ -31,6 +34,19 @@ public class SpeakerRepositoryImpl implements SpeakerRepository {
     @Override
     public Speaker create(Speaker speaker) {
         //jdbcTemplate.update("INSERT INTO speaker (name) VALUES (?)", speaker.getName());
+        //using SimpleJdbcInsert
+        SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+        insert.setTableName("speaker"); //table we want to be working with
+
+        List<String> columns = new ArrayList<>();
+        columns.add("name");
+
+        Map<String, Object> data = new HashMap<>();
+        data.put("name", speaker.getName()); //this is the data we want to insert
+
+        insert.setGeneratedKeyName("id");
+        Number key = insert.executeAndReturnKey(data);
+        System.out.println(key);
         return null;
     }
 }
